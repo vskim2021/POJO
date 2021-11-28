eb85b0b3754f91b21f38cfcba02f2f013c256a05
vskim <kimbyungsoo@outlook.com>
2021-11-28 16:29:50 +0900

    first commit

---
+++ src/com/company/KCities.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KCities {
+
+    static List<String> cities = Arrays.asList("seoul", "tokyo", "sydney", "london");
+
+    public static void main(String[] args) {
+        boolean found = false;
+
+        for (String city : cities) {
+            System.out.println(city);
+        }
+    }
+}
